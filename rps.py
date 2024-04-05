32939057b934de5f66361eb677e2ff3bd5e7c210
Deepi-AV <avdeepi2003@gmail.com>
2024-04-05 19:47:31 +0530

    argparse concept

--- rps.py
+++ rps.py
@@ -1,6 +1,7 @@
 import random
 import sys
 from enum import Enum
+
 #Rock-Paper-Scissors
 
 
@@ -10,8 +11,11 @@ def rps(playerName="Player"):
     player_score=0
     computer_score=0
 
+    print(f"\nWelcome {playerName}!")
+    
     def play_rps():
 
+        nonlocal playerName
         nonlocal game_count
         nonlocal player_score
         nonlocal computer_score
@@ -20,7 +24,7 @@ def rps(playerName="Player"):
             ROCK= '1'
             PAPER= '2'
             SCISSORS= '3'
-    
+
         valid_inputs = ['1', '2', '3']
         playerCh=input("\nEnter 1 for Rock\n2 for Paper\n3 for Scissors\n\n")
         computerCh= random.choice("123")
@@ -29,21 +33,21 @@ def rps(playerName="Player"):
             print("You entered a wrong input...need to choose from 1,2, or 3")
             return play_rps()
         
-        print(playerName+" chose "+RPS(playerCh).name)
-        print("Computer chose "+RPS(computerCh).name)
+        print(f"{playerName} chose {RPS(playerCh).name}")
+        print(f"Computer chose {RPS(computerCh).name}")
 
         def decide_winner(player,computer):
             nonlocal player_score
             nonlocal computer_score
             if player=='1' and computer=='3':
                 player_score+=1
-                return playerName+" win!!🎊\n"
+                return f"{playerName} win!!🎊\n"
             elif player=='2' and computer=='1':
                 player_score+=1
-                return playerName+" win!!🎊\n"
+                return f"{playerName} win!!🎊\n"
             elif player=='3' and computer=='2':
                 player_score+=1
-                return playerName+" win!!🎊\n"
+                return f"{playerName} win!!🎊\n"
             elif player==computer:
                 return "Tie Game..😲\n"
             else:
@@ -65,18 +69,38 @@ def rps(playerName="Player"):
         else:
 
             if(player_score>computer_score):
-                print("Overall Winner "+playerName+" with "+str(player_score)+"/"+str(game_count))
+                print(f"Overall Winner {playerName} with {player_score} / {game_count}")
             elif(computer_score>player_score):
-                print("Overall Winner -COMPUTER with "+str(computer_score)+"/"+str(game_count))
+                print(f"Overall Winner -COMPUTER with {computer_score} / {game_count}")
             else:
-                print("Equal Score "+ playerName+"("+str(player_score)+") COMPUTER("+str(computer_score)+")")
+                print(f"Equal Score {playerName} ({player_score}) COMPUTER({computer_score})")
             return
 
 
         
     return play_rps
 
-name=input("Player Name: ")
-rock_paper_scisssors=rps(name)
+name=input("Enter player name: ")
+rock_paper_scissors=rps(name)
+
+
+#if __name__=="__main__":
+
+# import argparse
+
+# parser=argparse.ArgumentParser(
+#     description="Personalized game experience"
+# )
+
+# parser.add_argument(
+#     "-n","--name",metavar="name", required=True,help="Provide your name."
+# )
+
+# args=parser.parse_args()
+
+# rock_paper_scissors=rps(args.name)
+# print(f"\nWelcome, {args.name}! ")
+
+# rock_paper_scissors()
 
 
