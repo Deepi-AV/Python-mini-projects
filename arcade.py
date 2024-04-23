f238180cb6a5f82b739b5729961f37b72ee96d4a
Deepi-AV <avdeepi2003@gmail.com>
2024-04-23 22:48:16 +0530

    dbmsProject-CRUD operations

--- arcade.py
+++ arcade.py
@@ -1,28 +1,30 @@
 import rps
-import guess_the_number as gn 
+import guess_the_number as gn
 import sys
 
+
 def arcade_game(name="Player"):
-     
+
     print(f"\nWelcome to the arcade game..{name}!!\n")
 
     def play_arcade():
         nonlocal name
-        choice=input("Please Choose the game you would like to play:\n1 = Rock, Paper, and Scissors\n2 = Guess the Number\nOr Press x to Exit\n\nEnter: ")
+        choice = input(
+            "Please Choose the game you would like to play:\n1 = Rock, Paper, and Scissors\n2 = Guess the Number\nOr Press x to Exit\n\nEnter: ")
 
         def decide_game():
             nonlocal name
-            if choice=='1':
-                play_rps=rps.rps(name)
+            if choice == '1':
+                play_rps = rps.rps(name)
                 play_rps()
                 print(f"{name}, Welcome Back to the Arcade Menu..\n")
                 return play_arcade()
-            elif choice=='2':
-                play=gn.guess_Num(name)
+            elif choice == '2':
+                play = gn.guess_Num(name)
                 play()
                 print(f"{name}, Welcome Back to the Arcade Menu..\n")
                 return play_arcade()
-            elif choice=='x':
+            elif choice == 'x':
                 sys.exit(f"\nSee you soon, {name}!!\n")
             else:
                 print("You entered the wrong input...Choose from 1,2 or x")
@@ -33,19 +35,19 @@ def arcade_game(name="Player"):
     return play_arcade
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     import argparse
 
-    parser=argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description="Personalized game experience"
     )
 
     parser.add_argument(
-        "-n","--name",metavar="name", required=True,help="Provide your name."
+        "-n", "--name", metavar="name", required=True, help="Provide your name."
     )
 
-    args=parser.parse_args()
+    args = parser.parse_args()
 
-    play=arcade_game(args.name)
+    play = arcade_game(args.name)
     play()
